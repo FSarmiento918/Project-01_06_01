0d4c30187110b380b3be6cc3e82825d479200b53
Fernando Sarmiento <FSarmiento918@west-mec.org>
2018-08-15 18:25:51 -0700

    Initial Code Commit
    Created repo with all the necessary code files

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+      Project 01_06_01
+
+      Author: Fernando Sarmiento
+      Date:   08.15.18
+
+      Filename: script.js
+*/
+"use strict";
+
+var formValidity = true;
+
+
+
+
+
+
+function createEventListeners() {
+    window.addEventListener("load", function () {
+        alert("something");
+    })
+}
