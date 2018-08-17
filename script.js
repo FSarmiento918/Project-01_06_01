c9b73bb2b7f74876088e30f7215cb3380e874bd2
Fernando Sarmiento <FSarmiento918@west-mec.org>
2018-08-16 17:40:58 -0700

    Second Code Commit
    set it so that the page holds back from submission if there is an error
    *mistake* i accidentally put in project 2 code before pushing

--- script.js
+++ script.js
@@ -11,12 +11,68 @@
 var formValidity = true;
 
 
-
-
-
-
-function createEventListeners() {
-    window.addEventListener("load", function () {
-        alert("something");
-    })
+//function to validate the form
+function validateForm(evt) {
+    if (evt.preventDefault) {
+        evt.preventDefault();
+    } else {
+        evt.returnValue = false;
+    }
+    validateRequired();
+//    validateNumbers();
 }
+
+////remove placeholder text
+//function zeroPlaceholder() {
+//    var addressBox = document.getElementById("addrinput");
+//    addressBox.style.color = "black";
+//    if (addressBox.value === addressBox.placeholder) {
+//        addressBox.value === "";
+//    }
+//}
+//
+////function to restore placeholder text if box contains no user entry
+//function checkPlaceholder() {
+//    var addressBox = 
+//}
+
+//function to validate the required sections of the form
+function validateRequired() {
+    var formInput = document.getElementsByTagName("input");
+    var errorDiv = document.getElementById("errorText");
+    var fieldsetValidity = true;
+    var elementCount = formInput.length;
+    var currentElement = null;
+    try {
+        for (var i = 0; i < elementCount; i++) {
+            currentElement = formInput[i];
+            if (currentElement.value === "") {
+                currentElement.style.background = "rgb(255,233,233)";
+                fieldsetValidity = false;
+            } else {
+                currentElement.style.background = "white";
+            }
+        }
+        if (fieldsetValidity === false) {
+            throw "Please fill out the required fields";
+        } else {
+            errorDiv.style.display = "none";
+            errorDiv.innerHTML = "";
+            formValidity = true;
+        }
+    } catch (msg) {
+        errorDiv.style.display = "block";
+        errorDiv.innerHTML = msg;
+        formValidity = false;
+    }
+    if (formValidity === true) {
+        document.getElementsByTagName("form")[0].submit();
+    }
+}
+
+//function to create event listeners for the document
+function createEventListeners() {
+    window.addEventListener("submit", validateForm);
+}
+
+window.addEventListener("load", createEventListeners);
